f1f90e6a770a3130dd4b1ae8d181b38607eda193
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2016-07-09 18:13:00 +0200

    core: minor code cleanup

--- app/src/main/java/org/faudroids/werewolf/core/GameManager.java
+++ app/src/main/java/org/faudroids/werewolf/core/GameManager.java
@@ -5,17 +5,13 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.faudroids.werewolf.R;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -87,17 +83,6 @@ public class GameManager {
 			if (player.getName().equalsIgnoreCase(name)) return player;
 		}
 		return null;
-	}
-
-
-	public void createRandomTestPlayers(int playerCount) {
-		Random random = new Random();
-		List<Player> players = new ArrayList<>();
-		for(int i = 0; i < playerCount; ++i) {
-			int randomRole = random.nextInt(Role.values().length);
-			players.add(new Player(i, false, Role.values()[randomRole], context.getString(R.string.default_player_name, (i + 1))));
-		}
-		savePlayers(players);
 	}
 
 }
